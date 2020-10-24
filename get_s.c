e701e8b5b5d9d7d2fb0cc19c2abdd65578ca9e4f
lemon5227 <yan2000926@gamil.com>
2020-10-24 18:25:08 +0800

    351

---
+++ get_s.c
@@ -0,0 +1,8 @@
+#include"stdio.h"
+int main(){
+    char words[5];
+    gets_s(words,5);
+    puts(words);
+
+    return 0;
+}
---
+++ nogo.c
@@ -0,0 +1,38 @@
+//不能直接比较两串字符是否相同，因为两串字符地址不同。但可以用strcmp()函数不叫，字符串相同返回0,否则返回非零值。
+#include<stdio.h>
+#include<string.h>
+#define ANSWER "Great"
+#define SIZE 40
+char * s_gets(char *st,int n);
+int main(){
+    char try[SIZE];
+
+    puts("Who is buried in Grant's tomb?");
+    s_gets(try,SIZE);
+    while (strcmp(try,ANSWER)!=0)
+    {
+        puts("No, that's wrong. Try again.");
+        s_gets(try,SIZE);
+    }
+    puts("That's right!");
+
+    return 0;
+    
+}
+char * s_gets(char * st,int n){
+    char * ret_val;
+    int i = 0;
+    ret_val = fgets(st,n,stdin);
+    if(ret_val){
+        while (st[i] != '\n'&&st[i] != '\0')
+        
+            i++;
+            if(st[i] =='\n')
+                st[i]='\0';
+            else
+                while (getchar() != '\n')
+                    continue;
+            
+    }
+    return ret_val;
+}
